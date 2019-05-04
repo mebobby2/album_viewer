3887cd5443a5ac9908706a02acd4163d53da3b97
Bobby Lei <bobby.lei@hreasily.com>
2019-05-04 22:18:37 +1200

    Do not run tests under node_modules

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,11 @@ module.exports = {
       statements: 90,
       statements: 90,
       statements: 90,
-    }
+    },
   },
   testPathIgnorePatterns: [
-    "<rootDir>/__tests__/setup.js"
+    "<rootDir>/__tests__/setup.js",
+    "<rootDir>/node_modules"
   ],
-  setupTestFrameworkScriptFile: "<rootDir>/__tests__/setup.js"
+  setupFilesAfterEnv: ["<rootDir>/__tests__/setup.js"],
 };
