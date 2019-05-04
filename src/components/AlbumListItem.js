ae6c4c21cc5238a4ec76e08a792735491af384a9
Bobby Lei <bobby.lei@hreasily.com>
2019-05-04 23:02:22 +1200

    Fix flow import types

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { View, Text } from "react-native";
-import { Album, User } from "../types";
+import type { Album, User } from "../types";
 
 interface Props {
   album: Album;
