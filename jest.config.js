73400a61b6f7d2895d55215b4962a19ee6d3aedc
Bobby Lei <bobby.lei@hreasily.com>
2019-05-04 22:11:32 +1200

    Setup enzyme

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "react-native",
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      statements: 90,
+      statements: 90,
+      statements: 90,
+    }
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/__tests__/setup.js"
+  ],
+  setupTestFrameworkScriptFile: "<rootDir>/__tests__/setup.js"
+};
